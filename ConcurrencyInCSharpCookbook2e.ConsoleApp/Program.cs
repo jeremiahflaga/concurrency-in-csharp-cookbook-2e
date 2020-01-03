8dd41e22de5494788a9743f30202ffb1550279c0
Jeremiah Flaga <j.flaga@arcanys.com>
2020-01-03 20:06:17 +0800

    created console app and WPF app to test chapter 1 code: failed, because the first code example is supposed to fail but it is not failing; I will start with ch 2

---
+++ ConcurrencyInCSharpCookbook2e.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConcurrencyInCSharpCookbook2e.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                await new ch01_console().Execute();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ConcurrencyInCSharpCookbook2e.ConsoleApp/ch01_console.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrencyInCSharpCookbook2e.ConsoleApp
+{
+    class ch01_console
+    {
+        public async Task Execute()
+        {
+            //await new ch01_01().DoSomethingAsync();
+            //await new ch01_02().DoSomethingAsync();
+        }
+    }
+}
